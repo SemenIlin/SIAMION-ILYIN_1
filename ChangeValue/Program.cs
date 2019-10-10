5ff8462a3a6a3edc7bba50042a183e5b98aa3464
SIAMION ILYIN <npo100sam@gmail.com>
2019-10-10 21:09:31 +0300

    Added ChangeValue task

---
+++ ChangeValue/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ChangeValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value1 = "";
+            string value2 = "";
+
+            Operations operation = new Operations();
+
+
+            operation.Input();
+
+            value1 = operation.Value1FromUser;
+
+            value2 = operation.Value2FromUser;
+
+            operation.Change(ref value1,ref value2);
+
+            operation.Show(value1, value2);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ChangeValue/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ChangeValue")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ChangeValue")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("4bdbf4c9-3f4b-4bd7-b02a-1ec103af1941")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
